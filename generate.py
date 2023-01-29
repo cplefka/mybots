64145d43b03b6121d9945f4ab41162d73bbf91e2
cplefka <cplefka@uvm.edu>
2023-01-29 16:54:50 -0500

    created castle

--- generate.py
+++ generate.py
@@ -9,6 +9,16 @@ y = 0
 z = .5
 
 pyrosim.Start_SDF("boxes.sdf")
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length, width, height])
-pyrosim.Send_Cube(name="Box2", pos=[x+1,y,z+1] , size=[length, width, height])
+
+for a in range(5):
+    for b in range(5):
+        length = 1
+        width = 1
+        height = 1
+        for i in range(10):
+            pyrosim.Send_Cube(name="Box", pos=[x+a,y+b,z+i] , size=[length, width, height])
+            length = length * .9
+            width = width * .9
+            height = height * .9
+
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -9,7 +9,7 @@ p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 
 p.loadSDF("boxes.sdf") 
-for i in range(0,1000):
+for i in range(0,10000):
     p.stepSimulation()
     time.sleep(1/60)
     print(i)
