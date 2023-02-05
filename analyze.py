91f903feeb4456a0df7f138f889e93c1ec4a89b9
cplefka <cplefka@uvm.edu>
2023-02-05 13:30:22 -0500

    finished visualization

--- analyze.py
+++ analyze.py
@@ -5,6 +5,7 @@ backLegSensorVals = numpy.load("data/backLegSensorValues.npy")
 frontLegSensorVals = numpy.load("data/frontLegSensorValues.npy")
 
 
-matplotlib.pyplot.plot(backLegSensorVals)
-matplotlib.pyplot.plot(frontLegSensorVals)
+matplotlib.pyplot.plot(backLegSensorVals, label='Back Leg', linewidth = 5, color = "pink")
+matplotlib.pyplot.plot(frontLegSensorVals, label='Front Leg', color = "green")
+matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
