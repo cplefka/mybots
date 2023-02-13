f5f2453bc9bd076d4e43388fabab398f01cbb60a
cplefka <cplefka@uvm.edu>
2023-02-13 11:07:21 -0500

    make first motor with no movement

--- simulate.py
+++ simulate.py
@@ -20,8 +20,14 @@ for i in range(0,100):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    pyrosim.Set_Motor_For_Joint(
+    bodyIndex = robotId,
+    jointName = b'Torso_BackLeg',
+    controlMode = p.POSITION_CONTROL,
+    targetPosition = 0.0,
+    maxForce = 500)
     time.sleep(1/60)
 p.disconnect()
-print(backLegSensorValues)
+#print(backLegSensorValues)
 numpy.save("data/frontLegSensorValues.npy", frontLegSensorValues)
 numpy.save("data/backLegSensorValues.npy", backLegSensorValues)
