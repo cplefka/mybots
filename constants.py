ef64447653c9b0bc494771f516d38ad176e05521
cplefka <cplefka@uvm.edu>
2023-02-20 15:42:17 -0500

    added constants

---
+++ constants.py
@@ -0,0 +1,16 @@
+#File full of constants used
+import numpy
+
+GRAV = -90.8
+
+PI = numpy.pi
+STEPS = 800
+
+
+amplitudeF = PI/4
+frequencyF = 20 
+phaseOffsetF = 0
+
+amplitudeB = PI/4
+frequencyB = 20 
+phaseOffsetB = PI/4
