9b339ce42a54642a713bd3b2e5844fe74984f2ef
cplefka <cplefka@uvm.edu>
2023-02-20 15:42:52 -0500

    constants

--- simulate.py
+++ simulate.py
@@ -3,40 +3,31 @@ import pybullet as p
 import time
 import pybullet_data
 import numpy
-import random 
-
-pi = numpy.pi
-STEPS = 800
+import random
+import constants as c 
 
 
 physicsClient = p.connect(p.GUI)
 p.configureDebugVisualizer(p.COV_ENABLE_GUI,0) 
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-p.setGravity(0,0,-90.8)
+p.setGravity(0,0,c.GRAV)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 
 p.loadSDF("world.sdf") 
 pyrosim.Prepare_To_Simulate(robotId)
-backLegSensorValues = numpy.zeros(STEPS)
-frontLegSensorValues = numpy.zeros(STEPS)
+backLegSensorValues = numpy.zeros(c.STEPS)
+frontLegSensorValues = numpy.zeros(c.STEPS)
 
-numsArray = 2*pi*(numpy.arange(STEPS) / STEPS)
-targetAngles = (pi/4)*numpy.sin(numsArray)
+numsArray = 2*c.PI*(numpy.arange(c.STEPS) / c.STEPS)
+targetAngles = (c.PI/4)*numpy.sin(numsArray)
 
-amplitudeF = pi/4
-frequencyF = 20 
-phaseOffsetF = 0
 
-amplitudeB = pi/4
-frequencyB = 20 
-phaseOffsetB = pi/4
+targetAnglesF = c.amplitudeF * numpy.sin(c.frequencyF * numsArray +c.phaseOffsetF)
+targetAnglesB = c.amplitudeB * numpy.sin(c.frequencyB * numsArray +c.phaseOffsetB)
 
-targetAnglesF = amplitudeF * numpy.sin(frequencyF * numsArray +phaseOffsetF)
-targetAnglesB = amplitudeB * numpy.sin(frequencyB * numsArray +phaseOffsetB)
-
-for i in range(0,STEPS):
+for i in range(0,c.STEPS):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
