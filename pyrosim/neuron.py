ea2b70c9a88add1b03dba7d3df1a54e26cd72748
cplefka <cplefka@uvm.edu>
2023-03-06 11:58:59 -0500

    multiple synapses

--- pyrosim/neuron.py
+++ pyrosim/neuron.py
@@ -75,6 +75,7 @@ class NEURON:
                 self.Allow_Presynaptic_Neuron_To_Influence_Me(
                     synapses[key].Get_Weight(),
                     neurons[key[0]].Get_Value())
+        self.Threshold()
 
 
     def Allow_Presynaptic_Neuron_To_Influence_Me(self, weight, value):
