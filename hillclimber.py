e33a3538dafc1bc27ec4eca8e8417e3ca4ecd805
cplefka <cplefka@uvm.edu>
2023-03-13 18:49:48 -0400

    blind running

--- hillclimber.py
+++ hillclimber.py
@@ -10,3 +10,27 @@ class HILL_CLIMBER:
         self.parent.Evaluate()
         for currentGeneration in range(c.numberOfGenerations):
             self.Evolve_For_One_Generation()
+            
+    def Evolve_For_One_Generation(self):
+        self.Spawn()
+        self.Mutate()
+        self.child.Evaluate("DIRECT")
+        self.Print()
+        self.Select()
+
+    def Spawn(self):
+        self.child = copy.deepcopy(self.parent)
+
+    def Mutate(self):
+        self.child.Mutate()
+
+    def Select(self):
+        if (self.parent.fitness > self.child.fitness):
+            self.parent = self.child
+    
+    def Print(self):
+        print("fitness coordinates for this run:")
+        print(self.parent.fitness, self.child.fitness)
+
+    def Show_Best(self):
+        self.parent.Evaluate("GUI")
